189b6384c63b88a8829f6bbf690cd38562bbbfa8
ASHINA3 <upa_shop@yahoo.co.jp>
2021-09-26 15:11:38 +0900

    設定：新規登録機能・バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :encrypted_password, :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ }
+
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
