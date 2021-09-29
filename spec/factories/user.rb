bc839700e347786c4bdff15afd82576ca5c41a82
ASHINA3 <upa_shop@yahoo.co.jp>
2021-09-29 16:03:00 +0900

    コードレビュー修正・再設定：Faker・パスワードに英数字を必ず混同させる

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
     nickname { Faker::Name.initials(number: 3) }
     email { Faker::Internet.free_email }
-    password { Faker::Lorem.characters(number: 7, min_alpha: 1, min_numeric: 1) }
+    password { '1a' + Faker::Lorem.characters(number: 7, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
     last_name { person.last.kanji }
     first_name { person.first.kanji }
