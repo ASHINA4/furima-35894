232ad061a21ec84f8c4e07726fa6732f8aa11bde
ASHINA3 <upa_shop@yahoo.co.jp>
2021-10-01 15:58:25 +0900

    設定：商品出品機能・/items・ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  # デバイスのルーティング
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # トップページビュー表示のルーティング
   root to: 'items#index'
+
+  # /itemsのパスに対応するルーティング
+  resources :items, only: [ :new, :create ]
+
 end
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
