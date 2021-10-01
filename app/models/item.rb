72531361eea6e5b51b1eb81d434d03534e76f92d
ASHINA3 <upa_shop@yahoo.co.jp>
2021-10-01 12:31:35 +0900

    設定：商品出品機能・itemモデル・バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   # アソシエーションの設定
   belongs_to :user
-end
+
+  # バリデーションの設定
+  with_options presence: true do
+    validates :image
+    validates :name, uniqueness: true, length: { maxmum: 100 }
+    validates :info, uniqueness: true, length: { maxmum: 1000 }
+    validates :category_id
+    validates :ssales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 },
+              format: { with: /\A[0-9]+\z/ }
+  end
