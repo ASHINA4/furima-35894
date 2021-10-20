a9c7d0b39d4629bcbf3254bdeeeb092c6b0a0580
ASHINA3 <upa_shop@yahoo.co.jp>
2021-10-20 16:46:01 +0900

    実装：商品削除機能・商品詳細ページからの商品削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
 
   def index
@@ -24,6 +24,7 @@ class ItemsController < ApplicationController
   def show
   end
 
+  
   def edit
     unless
       @item.user_id == current_user.id
@@ -41,7 +42,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    
+    @item.destroy
+    redirect_to root_path
   end
 
   private
