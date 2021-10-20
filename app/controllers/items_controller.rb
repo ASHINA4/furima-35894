2b4ec02d65affc2321bc18366382be903507051e
ASHINA3 <upa_shop@yahoo.co.jp>
2021-10-20 12:03:18 +0900

    コードレビュー修正：設定・商品情報編集機能・itemsコントローラー・共通のアクションをbefore_actionにまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
 
   def index
@@ -21,11 +22,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless
       @item.user_id == current_user.id
       redirect_to action: :index
@@ -33,7 +32,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if
       @item.update(item_params)
       redirect_to item_path
@@ -47,5 +45,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id,
                                  :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
