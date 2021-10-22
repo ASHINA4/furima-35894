8562e5444fc72f479e3235f76499b485d96c8c8c
ASHINA3 <upa_shop@yahoo.co.jp>
2021-10-23 02:12:24 +0900

    設定：商品購入機能・ActiveHash・アソシエーション・addressモデルとprefectureモデル

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,9 @@ class Address < ApplicationRecord
   # テーブル同士のアソシエーション
   has_one_attached :order
   # /テーブル同士のアソシエーション
+
+  # ActiveHashのモデルファイルとのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  # /ActiveHashのモデルファイルとのアソシエーション
 end
