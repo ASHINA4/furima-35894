1a6a8002af91224c1ad5eb47960bfaea8a2b6373
ASHINA3 <upa_shop@yahoo.co.jp>
2021-10-23 02:10:04 +0900

    設定：商品購入機能・アソシエーション・orderモデルとaddressモデル

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,5 @@
 class Address < ApplicationRecord
+  # テーブル同士のアソシエーション
+  has_one_attached :order
+  # /テーブル同士のアソシエーション
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,6 @@ class Order < ApplicationRecord
   # テーブル同士のアソシエーション
   belongs_to :user
   belongs_to :item
+  has_one_attached :address
   # /テーブル同士のアソシエーション
 end
