a7010d2ef892072b6d6475194f996e9f10e437af
ASHINA3 <upa_shop@yahoo.co.jp>
2021-10-23 02:26:21 +0900

    作成：商品購入機能・Formオブジェクト・order_addressモデル

---
+++ app/models/order_address.rb
@@ -0,0 +1,25 @@
+class OrderAddress
+  # form_withとのバリデーション設定
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id
+  # /form_withとのバリデーション設定
+
+  # addressのバリデーション設定
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :city
+    validates :block
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/ }
+  end
+  # /addressのバリデーション設定
+
+  # saveメソッドに保存するテーブルとカラムの設定
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id)
+  end
+  # /saveメソッドに保存するテーブルとカラムの設定
+end
