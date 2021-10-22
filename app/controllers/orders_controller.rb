f2532581277b3709786f94f1c786541d69822a19
ASHINA3 <upa_shop@yahoo.co.jp>
2021-10-23 03:27:41 +0900

    設定：商品購入機能・ordersコントローラー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,33 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    if
+      @item.user_id == current_user.id
+      redirect_to root_path
+    else
+      @order_address = OrderAddress.new
+    end
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path 
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
