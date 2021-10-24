fcded69e0b3088651ce32d91f1ca20953413df58
ASHINA3 <upa_shop@yahoo.co.jp>
2021-10-24 15:47:14 +0900

    設定：クレジットカード決済機能・order_addressモデル・トークン値

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,13 +1,14 @@
 class OrderAddress
   # form_withとのバリデーション設定
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id
+  attr_accessor :token, :postal_code, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id
   # /form_withとのバリデーション設定
 
   # addressのバリデーション設定
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
